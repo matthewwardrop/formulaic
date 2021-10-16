dc6e38da3ce7a4e17c030b1a584a8ee489a2f8df
Matthew Wardrop <mpwardrop@gmail.com>
2021-10-15 22:04:36 -0700

    Add support for the `poly` transform.

--- formulaic/transforms/__init__.py
+++ formulaic/transforms/__init__.py
@@ -1,12 +1,14 @@
 from .basis_spline import basis_spline
 from .identity import identity
 from .encode_categorical import encode_categorical
+from .poly import poly
 from .scale import center, scale
 
 
 TRANSFORMS = {
     "bs": basis_spline,
     "center": center,
+    "poly": poly,
     "scale": scale,
     "C": encode_categorical,
     "I": identity,
