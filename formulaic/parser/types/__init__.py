9c9107fd039a505c379b80349efbd48df0a7416e
Matthew Wardrop <mpwardrop@gmail.com>
2022-03-14 11:10:46 +1100

    Add `Structured` as a container for structured formulae/terms/model matrices/etc.

--- formulaic/parser/types/__init__.py
+++ formulaic/parser/types/__init__.py
@@ -2,8 +2,17 @@ from .ast_node import ASTNode
 from .factor import Factor
 from .operator import Operator
 from .operator_resolver import OperatorResolver
+from .structured import Structured
 from .term import Term
 from .token import Token
 
 
-__all__ = ["ASTNode", "Factor", "Operator", "OperatorResolver", "Term", "Token"]
+__all__ = [
+    "ASTNode",
+    "Factor",
+    "Operator",
+    "OperatorResolver",
+    "Structured",
+    "Term",
+    "Token",
+]
