e0e2677bc7a6514455e4590d9099d4ca132be2fd
Matthew Wardrop <mpwardrop@gmail.com>
2022-03-17 09:01:46 +1100

    Skip coverage of numpy type-checking exception.

--- formulaic/transforms/poly.py
+++ formulaic/transforms/poly.py
@@ -11,7 +11,7 @@ try:
     import numpy.typing
 except ImportError as e:
     if TYPE_CHECKING:
-        raise RuntimeError("Numpy >=1.20 is required for type-checking.") from e
+        raise RuntimeError("Numpy >=1.20 is required for type-checking.") from e  # pragma: no cover
 
 
 @stateful_transform
