5023114453cff0d68b161801c0f11ae9d980dcc3
Matthew Wardrop <mpwardrop@gmail.com>
2022-07-31 20:17:09 -0700

    Document and annotate `ModelMatrix`.

--- formulaic/model_matrix.py
+++ formulaic/model_matrix.py
@@ -1,13 +1,38 @@
+from __future__ import annotations
+
+from typing import Any, Optional, TYPE_CHECKING
+
 import wrapt
+
+if TYPE_CHECKING:  # pragma: no cover
+    from .model_spec import ModelSpec
 
 
 class ModelMatrix(wrapt.ObjectProxy):
-    def __init__(self, data, spec=None):
-        wrapt.ObjectProxy.__init__(self, data)
+    """
+    A wrapper around arbitrary model matrix output representations.
+
+    This wrapper allows for `isinstance(..., ModelMatrix)` checks, and allows
+    one to access the `ModelSpec` instance associated with its creation using
+    `<model_matrix>.model_spec`. All other instance attributes and methods of
+    the wrapped object are directly accessible as if the object were unwrapped.
+    """
+
+    def __init__(self, matrix: Any, spec: Optional[ModelSpec] = None):
+        wrapt.ObjectProxy.__init__(self, matrix)
         self._self_model_spec = spec
 
     @property
-    def model_spec(self):
+    def model_spec(self) -> Optional[ModelSpec]:
+        """
+        The `ModelSpec` instance associated with the creation of this
+        `ModelMatrix` instance.
+
+        This `ModelSpec` instance can be used to create other `ModelMatrix`s
+        that respect all the choices (including feature selection and encoding)
+        that were made in the construction of this `ModelMatrix` instance.
+
+        """
         return self._self_model_spec
 
     def __repr__(self):
