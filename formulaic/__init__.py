d2e0b69313c1fc44ac580ba9014d10a98f270c74
Matthew Wardrop <mpwardrop@gmail.com>
2022-08-28 14:02:05 -0700

    Export `FormulaSpec` from the top-level `formulaic` module.

--- formulaic/__init__.py
+++ formulaic/__init__.py
@@ -1,5 +1,5 @@
 from ._version import __author__, __author_email__, __version__
-from .formula import Formula
+from .formula import Formula, FormulaSpec
 from .materializers import FactorValues
 from .model_matrix import ModelMatrix, ModelMatrices
 from .model_spec import ModelSpec, ModelSpecs
@@ -10,6 +10,7 @@ __all__ = [
     "__author_email__",
     "__version__",
     "Formula",
+    "FormulaSpec",
     "ModelMatrix",
     "ModelMatrices",
     "ModelSpec",
