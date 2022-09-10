691ce3f51aec4bfa4196b02e9da073ecabab12a8
Matthew Wardrop <mpwardrop@gmail.com>
2022-09-09 19:21:29 -0700

    Fix updating of `formulaic.__version__` during build.

--- formulaic/__init__.py
+++ formulaic/__init__.py
@@ -1,14 +1,23 @@
-from ._version import __author__, __author_email__, __version__
 from .formula import Formula, FormulaSpec
 from .materializers import FactorValues
 from .model_matrix import ModelMatrix, ModelMatrices
 from .model_spec import ModelSpec, ModelSpecs
 from .sugar import model_matrix
 
+try:
+    from ._version import __version__, __version_tuple__
+except ImportError:  # pragma: no cover
+    __version__ = version = "unknown"
+    __version_tuple__ = version_tuple = ("unknown",)
+
+__author__ = "Matthew Wardrop"
+__author_email__ = "mpwardrop@gmail.com"
+
 __all__ = [
     "__author__",
     "__author_email__",
     "__version__",
+    "__version_tuple__",
     "Formula",
     "FormulaSpec",
     "ModelMatrix",
