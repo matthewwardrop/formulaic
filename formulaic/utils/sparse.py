a19d1f1e7386dce32bda8df0cb47d3ac41ea4648
Dmitry Balabka <dmitry.balabka@ecentria.com>
2022-09-26 23:32:38 +0300

    Fix sparse matrix creation performance issue (#109)

--- formulaic/utils/sparse.py
+++ formulaic/utils/sparse.py
@@ -1,13 +1,14 @@
-from typing import Iterable, Optional
+from typing import Iterable, Optional, Tuple, List
 
 import numpy
+import numpy as np
 import pandas
 import scipy.sparse as spsparse
 
 
 def categorical_encode_series_to_sparse_csc_matrix(
     series: Iterable, levels: Optional[Iterable[str]] = None, drop_first: bool = False
-) -> spsparse.csc_matrix:
+) -> Tuple[List[str], spsparse.csc_matrix]:
     """
     Categorically encode (via dummy encoding) a `series` as a sparse matrix.
 
@@ -19,27 +20,24 @@ def categorical_encode_series_to_sparse_csc_matrix(
             structural collinearity.
 
     Returns:
-        The sparse (column-major) matrix representation of the series dummy
-        encoding.
+        A tuple (levels, sparse_matrix), where levels contain levels that were used to generate dummies,
+        and sparse_matrix is the sparse (column-major) matrix representation of the series dummy encoding.
     """
-    df = pandas.DataFrame(
-        {"series": pandas.Series(series).astype("category").reset_index(drop=True)}
-    )
-    levels = list(levels or df.series.cat.categories)
+
+    series = pandas.Series(series).astype("category").reset_index(drop=True)
+    levels = list(levels or series.cat.categories)
     if drop_first:
         levels = levels[1:]
-    results = df.groupby("series").groups.copy()
 
-    return levels, spsparse.hstack(
-        [
-            spsparse.csc_matrix(
-                (
-                    numpy.ones(len(indices), dtype=float),  # data
-                    (indices, numpy.zeros(len(indices), dtype=int)),  # row  # column
-                ),
-                shape=(numpy.array(series).shape[0], 1),
-            )
-            for level in levels
-            for indices in (results.get(level, []),)
-        ]
+    series = series.where(series.isin(levels))
+    codes, _ = pandas.factorize(series)
+    indices = numpy.arange(series.shape[0])[codes != -1]
+    codes = codes[codes != -1]
+    sparse_matrix = spsparse.csc_matrix(
+        (
+            numpy.ones(codes.shape[0], dtype=float),  # data
+            (indices, codes),  # row  # column
+        )
     )
+    return levels, sparse_matrix
+
