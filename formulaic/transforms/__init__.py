08bd3b937c8bb4789d868f2d5da2827080250100
Rishi Kulkarni <rishi@kulkarni.science>
2023-12-10 20:27:20 -0500

    Initial `hashed` implementation

--- formulaic/transforms/__init__.py
+++ formulaic/transforms/__init__.py
@@ -5,6 +5,7 @@ from formulaic.utils.stateful_transforms import stateful_transform
 from .basis_spline import basis_spline
 from .identity import identity
 from .contrasts import C, encode_contrasts, ContrastsRegistry
+from .featurehash import hashed
 from .patsy_compat import PATSY_COMPAT_TRANSFORMS
 from .poly import poly
 from .scale import center, scale
@@ -39,6 +40,7 @@ TRANSFORMS = {
     "C": C,
     "contr": ContrastsRegistry,
     "I": identity,
+    "hashed": hashed,
     # Patsy compatibility shims
     **PATSY_COMPAT_TRANSFORMS,
 }
